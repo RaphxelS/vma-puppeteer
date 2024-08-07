4ba167b131809dffd51cf37ad54aec0571c4f9f5
RaphxelS <raphael.scandella@epitech.eu>
2024-08-07 14:27:15 +0200

    fix: format

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { Worker } = require('worker_threads');
+const {Worker} = require('worker_threads');
 const fs = require('fs');
 const path = require('path');
 
@@ -8,21 +8,22 @@ const nbVotes = parseInt(args[1], 10);
 const numberOfWorkers = parseInt(args[2], 10);
 
 if (isNaN(votesPerWorker) || votesPerWorker <= 0) {
-    console.error('Invalid votes Per Worker. Please provide a positive integer.');
-    console.error('node main.js [voter] [nbVotes] [thread]');
-    process.exit(1);
+  console.error('Invalid votes Per Worker. Please provide a positive integer.');
+  console.error('node main.js [voter] [nbVotes] [thread]');
+  process.exit(1);
 }
 
 if (isNaN(nbVotes) || nbVotes <= 0) {
-    console.error('Invalid number of votes. Please provide a positive integer.');
-    console.error('node main.js [voter] [nbVotes] [thread]');
-    process.exit(1);
+  console.error('Invalid number of votes. Please provide a positive integer.');
+  console.error('node main.js [voter] [nbVotes] [thread]');
+  process.exit(1);
 }
 
 if (isNaN(numberOfWorkers) || numberOfWorkers <= 0) {
-    console.error('Invalid number Of Workers. Please provide a positive integer.');
-    console.error('node main.js [voter] [nbVotes] [thread]');
-    process.exit(1);
+  console.error(
+      'Invalid number Of Workers. Please provide a positive integer.');
+  console.error('node main.js [voter] [nbVotes] [thread]');
+  process.exit(1);
 }
 
 
@@ -31,29 +32,29 @@ let emails = [];
 let completedWorkers = 0;
 
 const handleWorkerMessages = (worker, workerIndex) => {
-    worker.on('message', (mail) => {
-        emails.push(mail);
-    });
+  worker.on('message', (mail) => {
+    emails.push(mail);
+  });
 
-    worker.on('exit', () => {
-        completedWorkers++;
-        if (completedWorkers === numberOfWorkers) {
-            fs.appendFile('emails.txt',  emails.join('\n'), function (err) {
-                if (err) throw err;
-              });
-            console.log('All emails have been saved to emails.txt');
-        }
-    });
+  worker.on('exit', () => {
+    completedWorkers++;
+    if (completedWorkers === numberOfWorkers) {
+      fs.appendFile('emails.txt', emails.join('\n'), function(err) {
+        if (err) throw err;
+      });
+      console.log('All emails have been saved to emails.txt');
+    }
+  });
 
-    worker.on('error', (err) => {
-        console.error(`Worker ${workerIndex} encountered an error:`, err);
-    });
+  worker.on('error', (err) => {
+    console.error(`Worker ${workerIndex} encountered an error:`, err);
+  });
 };
 
 for (let i = 0; i < numberOfWorkers; i++) {
-    const worker = new Worker(path.resolve(__dirname, 'worker.js'), {
-        workerData: { votesPerWorker, workerIndex: i, nbVotes }
-    });
+  const worker = new Worker(
+      path.resolve(__dirname, 'worker.js'),
+      {workerData: {votesPerWorker, workerIndex: i, nbVotes}});
 
-    handleWorkerMessages(worker, i);
+  handleWorkerMessages(worker, i);
 }
